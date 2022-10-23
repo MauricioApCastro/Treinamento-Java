d2ee76231cc9860936eb5197fbcea362f94bc542
MauricioApCastro <castromac6367@gmail.com>
2022-10-23 20:49:24 -0300

    programa de aprovacao de aluno

---
+++ meu-primeiro-projeto-java/src/BoletimEstudantil.java
@@ -0,0 +1,16 @@
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal =  5;
+        if(mediaFinal > 6  ){
+            System.out.println("Aprovado");    
+        }else if(mediaFinal == 6 ){
+            System.out.println("Passou raspando!!");
+
+        }else{
+            System.out.println("Reprovado");
+        }
+
+        
+    }
+    
+}
